f2737b978c290f08718de21784fcdf8fe93f4849
Kiefer Chang <kiefer.chang@suse.com>
2021-05-28 11:56:05 +0800

    Add virtualmachineimages.harvesterhci.io validator
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- pkg/api/image/schema.go
+++ pkg/api/image/schema.go
@@ -5,16 +5,13 @@ import (
 	"github.com/rancher/steve/pkg/server"
 	"github.com/rancher/steve/pkg/stores/proxy"
 
-	"github.com/harvester/harvester/pkg/api/store"
 	"github.com/harvester/harvester/pkg/config"
 )
 
 func RegisterSchema(scaled *config.Scaled, server *server.Server, options config.Options) error {
 	t := schema.Template{
-		ID: "harvesterhci.io.virtualmachineimage",
-		Store: store.DisplayNameValidatorStore{
-			Store: proxy.NewProxyStore(server.ClientFactory, nil, server.AccessSetLookup),
-		},
+		ID:    "harvesterhci.io.virtualmachineimage",
+		Store: proxy.NewProxyStore(server.ClientFactory, nil, server.AccessSetLookup),
 	}
 	server.SchemaFactory.AddTemplate(t)
 	return nil
---
+++ pkg/webhook/resources/virtualmachineimage/virtualmachineimage.go
@@ -0,0 +1,66 @@
+package virtualmachineimage
+
+import (
+	"github.com/rancher/wrangler/pkg/webhook"
+	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/harvester/harvester/pkg/apis/harvesterhci.io"
+	"github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
+	ctlharvesterv1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
+	werror "github.com/harvester/harvester/pkg/webhook/error"
+	"github.com/harvester/harvester/pkg/webhook/types"
+)
+
+const (
+	fieldDisplayName = "spec.displayName"
+)
+
+func NewValidator(vmimages ctlharvesterv1.VirtualMachineImageCache) types.Validator {
+	return &virtualMachineImageValidator{
+		vmimages: vmimages,
+	}
+}
+
+type virtualMachineImageValidator struct {
+	types.DefaultValidator
+
+	vmimages ctlharvesterv1.VirtualMachineImageCache
+}
+
+func (v *virtualMachineImageValidator) Info() types.ValidatorInfo {
+	return types.ValidatorInfo{
+		GroupName:  harvesterhci.GroupName,
+		ObjectType: &v1beta1.VirtualMachineImage{},
+	}
+}
+
+func (v *virtualMachineImageValidator) Create(request *webhook.Request, newObj runtime.Object) error {
+	logrus.Debug("entering virtualMachineImageValidator.Create")
+	newImage := newObj.(*v1beta1.VirtualMachineImage)
+
+	if newImage.Spec.DisplayName == "" {
+		return werror.NewInvalidError("displayName is required", fieldDisplayName)
+	}
+
+	allImages, err := v.vmimages.List(newImage.Namespace, labels.Everything())
+	if err != nil {
+		return werror.NewInternalError(err.Error())
+	}
+	for _, image := range allImages {
+		if newImage.Name == image.Name {
+			continue
+		}
+		if newImage.Spec.DisplayName == image.Spec.DisplayName {
+			return werror.NewConflict("A resource with the same name exists")
+		}
+	}
+
+	return nil
+}
+
+func (v *virtualMachineImageValidator) Update(request *webhook.Request, oldObj runtime.Object, newObj runtime.Object) error {
+	logrus.Debug("entering virtualMachineImageValidator.Update")
+	return v.Create(request, newObj)
+}
