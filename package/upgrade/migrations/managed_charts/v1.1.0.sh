5bd0b0a2a40e66f80991b0c305aaae2756868ef3
Kiefer Chang <kiefer.chang@suse.com>
2023-04-21 17:52:07 +0800

    fix

--- package/upgrade/migrations/managed_charts/v1.1.0.sh
+++ package/upgrade/migrations/managed_charts/v1.1.0.sh
@@ -6,7 +6,7 @@ CHART_MANIFEST=$2
 patch_harvester_ignore_default_sc()
 {
 	# add ignoring resources when upgrading to match the pr (https://github.com/harvester/harvester-installer/pull/481)
-	yq e '.spec.diff.comparePatches = [{"apiVersion": "storage.k8s.io/v1", "kind": "StorageClass", "name": "harvester-longhorn", "jsonPointers":["/metadata/annotations"]}]' $CHART_MANIFEST -i
+	yq e '.spec.diff.comparePatches += [{"apiVersion": "storage.k8s.io/v1", "kind": "StorageClass", "name": "harvester-longhorn", "jsonPointers":["/metadata/annotations"]}]' $CHART_MANIFEST -i
 }
 
 case $CHART_NAME in
